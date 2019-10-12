a05a5e66a0d482d2a7fba905c122374b4c12d32b
Андрій Пастернак <pasternakandrjj@gmail.com>
2019-10-12 16:47:50 +0300

    Added authentication

--- MyMagazine/Controllers/HomeController.cs
+++ MyMagazine/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using MyMagazine.Models;
 using MyMagazine.Helpers;
+using System.Web.Security;
 
 namespace MyMagazine.Controllers
 {
@@ -22,7 +23,7 @@ namespace MyMagazine.Controllers
             IndexViewModel ivm = new IndexViewModel { PageInfo = pageInfo, Phones = phonesPerPages };
             return View(ivm);
         }
-
+        [Authorize]
         [HttpGet]
         public ActionResult Buy(int id)
         {
@@ -128,6 +129,84 @@ namespace MyMagazine.Controllers
 
 
             return View(xiaomis);
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // поиск пользователя в бд
+                User user = null;
+                using (UserContext db = new UserContext())
+                {
+                    user = db.Users.FirstOrDefault(u => u.Email == model.Email && u.Password == model.Password);
+
+                }
+                if (user != null)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Email, true);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Пользователя с таким логином и паролем нет");
+                }
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = null;
+                using (UserContext db = new UserContext())
+                {
+                    user = db.Users.FirstOrDefault(u => u.Email == model.Email);
+                }
+                if (user == null)
+                {
+                    // создаем нового пользователя
+                    using (UserContext db = new UserContext())
+                    {
+                        db.Users.Add(new User { Email = model.Email, Password = model.Password, Age = model.Age });
+                        db.SaveChanges();
+
+                        user = db.Users.Where(u => u.Email == model.Email && u.Password == model.Password).FirstOrDefault();
+                    }
+                    // если пользователь удачно добавлен в бд
+                    if (user != null)
+                    {
+                        FormsAuthentication.SetAuthCookie(model.Email, true);
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Пользователь с таким логином уже существует");
+                }
+            }
+
+            return View(model);
+        }
+        public ActionResult Logoff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ MyMagazine/Models/Models.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyMagazine.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
+    public class RegisterModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+    }
+}
